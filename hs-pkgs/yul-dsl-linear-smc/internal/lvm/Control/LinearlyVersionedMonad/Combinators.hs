f8ca7cea8a2bea288770f93118f5946b6ad1a437
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2025-01-04 17:38:42 +0200

    complete LVM combinators

--- hs-pkgs/yul-dsl-linear-smc/internal/lvm/Control/LinearlyVersionedMonad/Combinators.hs
+++ hs-pkgs/yul-dsl-linear-smc/internal/lvm/Control/LinearlyVersionedMonad/Combinators.hs
@@ -9,16 +9,16 @@ Portability : GHC2024
 
 = Description
 
-These are a set of common combinators for linearly versioned monad.
+The module provides a set of common combinators for working with linearly versioned monad.
 
-These combinators should be reexported by the contextualized 'Control.LinearlyVersionedMonad.LVM'.
+These combinators should be reexported by the contextualized module of 'Control.LinearlyVersionedMonad.LVM'.
 
 -}
 module Control.LinearlyVersionedMonad.Combinators
   ( embed
-  , toss, tossN, tossToUnit
+  , tossToUnit, toss, tossN
   , pass, pass_, passN, passN_
-  , with, with_
+  , with, with_, withN, withN_
   ) where
 -- constraints
 import Data.Constraint.Linear         (Dict (Dict))
@@ -32,72 +32,76 @@ import Data.TupleN                    (ConvertibleTupleN, TupleNtoNP, fromTupleN
 --------------------------------------------------------------------------------
 
 -- | Embed a value into the context of a LVM.
-embed :: forall ctx m v a. (ContextualEmbeddable ctx m a)
-      => a ⊸ LVM ctx v v (m a)
+embed :: forall ctx m v a.
+  (ContextualEmbeddable ctx m a)
+  => a ⊸ LVM ctx v v (m a)
 embed a = MkLVM \ctx -> let !(ctx', ma) = contextualEmbed ctx a in (Dict, ctx', ma)
 
 --------------------------------------------------------------------------------
 -- toss
 --------------------------------------------------------------------------------
 
--- | Toss a single value into a context unit.
-toss :: forall ctx v a m. (ContextualConsumable ctx a, ContextualEmbeddable ctx m ())
-     => a ⊸ LVM ctx v v (m ())
+-- | Toss a single value into a context-free unit.
+tossToUnit :: forall ctx v a.
+  (ContextualConsumable ctx a)
+  => a ⊸ LVM ctx v v ()
+tossToUnit x = MkLVM \ctx -> (Dict, contextualConsume ctx x, ())
+
+-- | Toss a single value into a contextual unit.
+toss :: forall ctx v a m.
+  (ContextualConsumable ctx a, ContextualEmbeddable ctx m ())
+  => a ⊸ LVM ctx v v (m ())
 toss x = MkLVM \ctx -> let ctx' = contextualConsume ctx x
                            !(ctx'', mu) = contextualEmbed ctx' ()
                        in (Dict, ctx'', mu)
 
--- | Toss a TupleN into the context of a LVM.
+-- | Toss a TupleN into a contextual unit.
 tossN :: forall ctx v tpl m.
-         ( ConvertibleTupleN tpl
-         , ContextualConsumable ctx (TupleNtoNP tpl)
-         , ContextualEmbeddable ctx m ()
-         )
-     => tpl ⊸ LVM ctx v v (m ())
+  ( ConvertibleTupleN tpl
+  , ContextualConsumable ctx (TupleNtoNP tpl)
+  , ContextualEmbeddable ctx m ()
+  ) => tpl ⊸ LVM ctx v v (m ())
 tossN tpl = MkLVM \ctx -> let ctx' = contextualConsume ctx (fromTupleNtoNP tpl)
                               !(ctx'', mu) = contextualEmbed ctx' ()
                           in (Dict, ctx'', mu)
-
--- | Toss a single value into a simple unit.
-tossToUnit :: forall ctx v a. (ContextualConsumable ctx a)
-     => a ⊸ LVM ctx v v ()
-tossToUnit x = MkLVM \ctx -> (Dict, contextualConsume ctx x, ())
 
 --------------------------------------------------------------------------------
 -- pass
 --------------------------------------------------------------------------------
 
 -- | Pass the copied data to the next process, then pass both the original data and the result to the next stage.
-pass :: forall ctx va vb a b. (ContextualDupable ctx a)
-     => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb (a, b)
+pass :: forall ctx va vb a b.
+  (ContextualDupable ctx a)
+  => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb (a, b)
 pass a mb = MkLVM \ctx ->
   let !(ctx', (a1, a2)) = contextualDup ctx a
       !(alteb, ctx'', b) = unLVM (mb a1) ctx'
   in (alteb, ctx'', (a2, b))
 
 -- | Pass the copied data to the next process, then pass the original data to the next stage and discard the restart.
-pass_ :: forall ctx va vb a b. (ContextualDupable ctx a, ContextualConsumable ctx b)
-     => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb a
+pass_ :: forall ctx va vb a b.
+  (ContextualDupable ctx a, ContextualConsumable ctx b)
+  => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb a
 pass_ a mb = MkLVM \ctx ->
   let !(ctx', (a1, a2)) = contextualDup ctx a
       !(alteb, ctx'', b) = unLVM (mb a1) ctx'
       ctx''' = contextualConsume ctx'' b
   in (alteb, ctx''', a2)
 
--- | 'pass' for TupleN.
-passN :: forall ctx va vb tpl b. (ConvertibleTupleN tpl, ContextualDupable ctx (TupleNtoNP tpl))
-      => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb (tpl, b)
+-- | Combinator 'pass' for TupleN.
+passN :: forall ctx va vb tpl b.
+  (ConvertibleTupleN tpl, ContextualDupable ctx (TupleNtoNP tpl))
+  => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb (tpl, b)
 passN tpl mb = MkLVM \ctx ->
   let !(ctx', (tpl1, tpl2)) = contextualDupTupleN ctx tpl
       !(alteb, ctx'', b) = unLVM (mb tpl1) ctx'
   in (alteb, ctx'', (tpl2, b))
 
--- | 'pass_' for TupleN.
+-- | Combinator 'pass_' for TupleN.
 passN_ :: forall ctx va vb tpl b.
-          ( ConvertibleTupleN tpl, ContextualDupable ctx (TupleNtoNP tpl)
-          , ContextualConsumable ctx b
-          )
-       => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb tpl
+  ( ConvertibleTupleN tpl, ContextualDupable ctx (TupleNtoNP tpl)
+  , ContextualConsumable ctx b
+  ) => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb tpl
 passN_ tpl mb = passN tpl mb >>= (\(tpl', b) -> tossToUnit b >> pure tpl')
 
 --------------------------------------------------------------------------------
@@ -105,16 +109,33 @@ passN_ tpl mb = passN tpl mb >>= (\(tpl', b) -> tossToUnit b >> pure tpl')
 --------------------------------------------------------------------------------
 
 -- | Process input @a@ without creating a copy of it then return the result @b@.
-with :: forall ctx va vb a b. ()
-     => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb b
+with :: forall ctx va vb a b.
+  a ⊸ (a ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb b
 with a mb = MkLVM \ctx ->
   let !(alteb, ctx', b) = unLVM (mb a) ctx
   in (alteb, ctx', b)
 
--- | Process input @a@ without creating a copy of it then discard the result.
-with_ :: forall ctx va vb tpl b. (ContextualConsumable ctx b)
-       => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb ()
-with_ tpl mb = MkLVM \ctx ->
+-- | Process input @a@ without creating a copy of it then discard the result @b@.
+with_ :: forall ctx va vb a b.
+  (ContextualConsumable ctx b)
+  => a ⊸ (a ⊸ LVM ctx va vb b) ⊸ (LVM ctx va vb ())
+with_ a mb = MkLVM \ctx ->
+  let !(alteb, ctx', b) = unLVM (mb a) ctx
+      ctx'' = contextualConsume ctx' b
+  in (alteb, ctx'', ())
+
+-- | Combinator 'with' for TupleN.
+withN :: forall ctx va vb tpl b.
+  tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb b
+withN tpl mb = MkLVM \ctx ->
+  let !(alteb, ctx', b) = unLVM (mb tpl) ctx
+  in (alteb, ctx', b)
+
+-- | Combinator 'with_' for TupleN.
+withN_ :: forall ctx va vb tpl b.
+  (ContextualConsumable ctx b)
+  => tpl ⊸ (tpl ⊸ LVM ctx va vb b) ⊸ LVM ctx va vb ()
+withN_ tpl mb = MkLVM \ctx ->
   let !(alteb, ctx', b) = unLVM (mb tpl) ctx
       ctx'' = contextualConsume ctx' b
   in (alteb, ctx'', ())
