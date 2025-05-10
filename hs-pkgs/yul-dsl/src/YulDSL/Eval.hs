55078e7f3d5cc3b83414230529cb6b3bb4cf4e0b
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2025-05-10 12:29:48 +0300

    Eval: minor comments adjustment

--- hs-pkgs/yul-dsl/src/YulDSL/Eval.hs
+++ hs-pkgs/yul-dsl/src/YulDSL/Eval.hs
@@ -55,14 +55,14 @@ evalYulCat s_ a_ = evalState (go s_ (pure a_)) initEvalState
     -- category
     go YulId  ma        = ma
     go (YulComp n m) ma = go n (go m ma)
-    -- -- monoidal category
+    -- monoidal category
     go (YulProd m n) mab = do
       (a, b) <- mab
       a' <- go m (pure a)
       b' <- go n (pure b)
       pure (a', b')
     go YulSwap mab = mab >>= \(a, b) -> pure (b, a)
-    -- -- cartesian category
+    -- cartesian category
     go (YulFork m n) ma = do
       b <- go m ma
       c <- go n ma
@@ -71,15 +71,14 @@ evalYulCat s_ a_ = evalState (go s_ (pure a_)) initEvalState
     go YulExr mab = mab >>= \(_, b) -> pure b
     go YulDis ma  = void ma
     go YulDup ma  = ma >>= \a -> pure (a, a)
-    -- -- Cartesian Closed
+    -- Cartesian Closed
     go YulApply mfa = mfa >>= \(a2b, a) -> go (a2b a) (pure a)
     go (YulCurry ab2c) ma = ma >>= \a -> pure \b -> YulEmb a `YulFork` YulEmb b >.> ab2c
-    -- -- co-cartesian category: create "new" objects
+    -- co-cartesian category: create "new" objects
     go YulAbsurd  _ = error "absurd"
     go (YulEmb b) ma = ma >> pure b
     go (YulMapHask g) mr = mr >>= \r -> pure \a -> YulEmb r >.> g (YulEmb a)
-    -- -- control flow
-    go (YulITE ct cf) mba = mba >>= \(BOOL t, a) -> if t then go ct (pure a) else go cf (pure a)
+    -- control flow
     go (YulSwitch cf cs cdef) a = do
       cid <- go cf a
       filter ((cid ==) . fst) cs & \case
@@ -89,7 +88,7 @@ evalYulCat s_ a_ = evalState (go s_ (pure a_)) initEvalState
     go (YulJmpU (_, f)) ma = ma >>= go f . pure
     go (YulJmpB p) ma = ma >>= \a -> pure (yulB_eval p a)
     go (YulCall _) _ = error "YulCall not supported" -- FIXME
-    -- -- storage primitives
+    -- storage primitives
     go YulSGet ma = ma >>= \a -> gets $ \s -> fromJust (fromWord =<< M.lookup a (store_map s))
     go YulSPut mav = mav >>= \(a, v) -> modify' $ \s -> s { store_map = M.insert a (toWord v) (store_map s) }
 
