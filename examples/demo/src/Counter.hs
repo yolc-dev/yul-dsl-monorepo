945ed46c6c5b7ea711a9df43e1a0443e5d0c4745
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2026-06-07 12:55:55 +0300

    now it crashes in getCounter

--- examples/demo/src/Counter.hs
+++ examples/demo/src/Counter.hs
@@ -2,6 +2,58 @@ module Counter where
 import Control.LinearlyVersionedMonad qualified as LVM
 import Prelude.YulDSL
 
+-- base
+import GHC.TypeLits                   (KnownNat, type (+))
+-- linear-base
+import Prelude.Linear                 (String, fromInteger, type (~))
+-- yul-dsl
+import YulDSL.Core
+--
+import Control.LinearlyVersionedMonad qualified as LVM
+import YulDSL.Haskell.LibLinearSMC
+
+
+-- | A Storage Hash-Map (SHMap) with a U256 root-key.
+newtype SHMap a b = SHMap U256
+
+-- | Create a storage hash-map with a root-key represented by a string.
+shmap' :: forall s a b. s ~ (a -> b) => String -> SHMap a b
+shmap' key = SHMap (fromInteger (bytesnToInteger (stringKeccak256 key)))
+
+-- | Get a storage reference from the storage hash-map.
+shmapRef' :: forall a b ie r v.
+  ( KnownNat v, YulO3 r a b
+  ) =>
+  SHMap a b ->
+  P'x ie r a ⊸
+  YulMonad v v r (P'x ie r (REF b))
+shmapRef' (SHMap key) a = LVM.do
+  key' <- embed key
+  LVM.pure (extendType'l (keccak256'l (merge'l (key', a))))
+
+-- | Get a value from the storage hash-map.
+shmapGet' :: forall a b ie r v.
+  ( YulO4 r a b (REF b)
+  , SReferenceable ie v r (REF b) b
+  ) =>
+  SHMap a b ->
+  P'x ie r a ⊸
+  YulMonad v v r (P'V v r b)
+shmapGet' m a = shmapRef' m a LVM.>>= sget
+
+
+-- | Get a value from the storage hash-map.
+shmapPut' :: forall a b ie r v.
+  ( YulO3 r a b
+  , SReferenceable ie v r (REF b) b
+  ) =>
+  SHMap a b ->
+  P'x ie r a ⊸
+  P'V v r b ⊸
+  YulMonad v (v + 1) r (P'V (v + 1) r ())
+shmapPut m a b = shmapRef m a LVM.>>= \s -> sput s b
+
+
 object = mkYulObject "Counter" yulNoop
   [ -- staticFn "getGlobalCounter" getGlobalCounter
   -- omniFn   "incGlobalCounter" incGlobalCounter
@@ -11,7 +63,7 @@ object = mkYulObject "Counter" yulNoop
 
 -- | Storage map of user counters
 counterMap :: SHMap ADDR U256
-counterMap = shmap "Yolc.Demo.Counter.Storage.Counter.PerUser"
+counterMap = shmap' "Yolc.Demo.Counter.Storage.Counter.PerUser"
 
 getCounter :: StaticFn (ADDR -> U256)
 getCounter = $lfn $ yulmonad'p
