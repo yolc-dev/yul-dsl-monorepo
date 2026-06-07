21c344994a3f2b0783ef702594b73c9a07fc795a
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 00:45:50 +0200

    wip

--- hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/LinearFn.hs
+++ hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/LinearFn.hs
@@ -24,8 +24,6 @@ import Prelude.Linear
 import YulDSL.Core
 -- yul-dsl-pure
 import YulDSL.Haskell.LibPure
--- (linearly-versioned-monoad)
-import Control.LinearlyVersionedMonad                qualified as LVM
 --
 import YulDSL.Haskell.Effects.LinearSMC.LinearYulCat
 import YulDSL.Haskell.Effects.LinearSMC.YulMonad
@@ -35,29 +33,6 @@ import YulDSL.Haskell.Effects.LinearSMC.YulPort
 -- Linear Non-Pure Effects
 ------------------------------------------------------------------------------------------------------------------------
 
-data StaticFn f where
-  MkStaticFn :: forall (eff :: LinearEffectKind) f xs b.
-                ( ClassifiedYulCatEffect eff, AssertStaticEffect eff
-                , EquivalentNPOfFunction f xs b
-                ) => NamedYulCat eff (NP xs) b ⊸ StaticFn f
-
-instance EquivalentNPOfFunction f xs b => ClassifiedYulCat (StaticFn f) StaticEffect (NP xs) b where
-  withClassifiedYulCat (MkStaticFn f) g = g f
-
-data OmniFn f where
-  MkOmniFn :: forall (eff :: LinearEffectKind) f xs b.
-              ( ClassifiedYulCatEffect eff, AssertOmniEffect eff
-              , EquivalentNPOfFunction f xs b
-              ) => NamedYulCat eff (NP xs) b ⊸ OmniFn f
-
-instance EquivalentNPOfFunction f xs b => ClassifiedYulCat (OmniFn f) OmniEffect (NP xs) b where
-  withClassifiedYulCat (MkOmniFn f) g = g f
-
-------------------------------------------------------------------------------------------------------------------------
--- Constructible Linear Functions
-------------------------------------------------------------------------------------------------------------------------
-
--- | Create classified linear kind of yul functions.
 class ConstructibleLinearFn fn (ie :: PortEffect) (oe :: PortEffect) where
   -- | Define a `YulCat` morphism from a yul port diagram.
   lfn' :: forall f xs b.
@@ -70,11 +45,3 @@ class ConstructibleLinearFn fn (ie :: PortEffect) (oe :: PortEffect) where
 
 instance ConstructibleLinearFn PureFn PurePort PurePort where
   lfn' cid f = MkPureFn (cid, decode'l f)
-
-instance ConstructibleLinearFn StaticFn (VersionedPort 0) (VersionedPort 0) where
-  lfn' cid f = MkStaticFn (cid, decode'l f)
-
-instance ConstructibleLinearFn StaticFn PurePort (VersionedPort 0) where
-  lfn' cid f = MkStaticFn (cid, decode'l f)
-
-
