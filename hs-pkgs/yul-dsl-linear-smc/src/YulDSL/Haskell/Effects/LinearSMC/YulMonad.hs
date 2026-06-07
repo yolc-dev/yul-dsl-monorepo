071246adcd0f9fe013069597dc31c237ca7cf7d4
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 00:13:22 +0200

    wip

--- hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/YulMonad.hs
+++ hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/YulMonad.hs
@@ -4,12 +4,8 @@ module YulDSL.Haskell.Effects.LinearSMC.YulMonad
     YulMonad, runYulMonad
   , ypure, yembed
     -- * Yul Monadic Diagrams
-  , YulCat'LVM (MkYulCat'LVM), YulCat'LPM (MkYulCat'LPM)
-  , yulmonad'v, yulmonad'p
-  , module Control.LinearlyVersionedMonad.Combinators
-  , Control.Functor.Linear.fmap
+  , yulmonad'p
   --
-  , withinPureY
   ) where
 import GHC.TypeLits                                  (KnownNat)
 -- linear-base
@@ -93,8 +89,6 @@ instance YulO2 r a => ContextualEmbeddable (YulMonadCtx r) (P'x eff r) a where
                                            in (MkYulMonadCtx ud', x'v)
 
 ------------------------------------------------------------------------------------------------------------------------
--- yulmonad'v
-------------------------------------------------------------------------------------------------------------------------
 
 -- | Monadic yul port diagrams for versioned input and yul monad output.
 newtype YulCat'LVM v1 vn r a b = MkYulCat'LVM (P'V v1 r a ⊸ YulMonad v1 vn r b)
@@ -142,24 +136,6 @@ instance forall g x xs b r a v1 vn.
   curryNP fNP x = curryNP @g @xs @b @(P'V v1 r) @(YulMonad v1 vn r) @(YulCat'LVV v1 v1 r a) @One
                     (\(MkYulCat'LVV fxs) -> fNP (MkYulCat'LVV (\a -> (consNP x (fxs a)))))
 
-yulmonad'v :: forall xs b r vd m1 m1b m2 m2b f' b'.
-  ( KnownNat vd
-  , YulO3 (NP xs) b r
-  -- m1, m1b, m2, m2b
-  , P'V         0 r ~ m1
-  , YulMonad 0 vd r ~ m1b
-  , YulCat'LVV 0  0 r (NP xs) ~ m2
-  , YulCat'LVM 0 vd r (NP xs) ~ m2b
-  -- b'
-  , P'V vd r b ~ b'
-  -- f'
-  , UncurriableNP f' xs b' m1 m1b m2 m2b One
-  ) =>
-  LiftFunction f' m1 m1b One ->  -- ^ LiftFunction               f1  m1 m1b One
-  (P'V 0 r (NP xs) ⊸ P'V vd r b) -- ^ LiftFunction (NP (():xs) -> b) m1 m1b One
-yulmonad'v f =
-  let !(MkYulCat'LVM f') = uncurryNP @f' @xs @b' @m1 @m1b @m2 @m2b @One f (MkYulCat'LVV id)
-  in \xs -> mkUnit'l xs & \(xs', u) -> runYulMonad u (f' xs')
 
 ------------------------------------------------------------------------------------------------------------------------
 -- yulmonad'p
@@ -213,29 +189,3 @@ yulmonad'p f =
 -- Run YulPorts Within a Pure Yul Function
 ------------------------------------------------------------------------------------------------------------------------
 
-withinPureY :: forall f x xs b r ioe m1 m2.
-  ( YulO4 x (NP xs) b r
-  -- m1, m2
-  , P'x ioe r ~ m1
-  , YulCat Pure (NP (x:xs)) ~ m2
-  -- f, x:xs, b
-  , EquivalentNPOfFunction f (x:xs) b
-  , ConvertibleNPtoTupleN (NP (MapList m1 (x:xs)))
-  , LinearDistributiveNP m1 (x:xs)
-  , UncurriableNP f (x:xs) b m2 m2 m2 m2 Many
-  ) =>
-  NPtoTupleN (NP (MapList m1 (x:xs))) ⊸
-  PureY f ->
-  P'x ioe r b
-withinPureY tplxxs f = encodeP'x cat' sxxs
-  where !(x, xs) = splitNonEmptyNP (fromTupleNtoNP tplxxs)
-        !(x', u) = mkUnit'l x
-        sxxs = linearDistributeNP (x' :* xs) u :: m1 (NP (x:xs))
-        cat = uncurryNP @f @(x:xs) @b @m2 @m2 @m2 @m2 f YulId
-        cat' = YulUnsafeCoerceEffect cat
-
--- cond :: P'V va r BOOL ⊸ (Bool ⊸ YulMonad va vb r (P'V vb r a))
--- cond c f = _
---   -- LVM.do
---   -- u <- yembed ()
---   -- match (YulId :: YulCat eff p p)
