fec58741883909066a5f40d08842a97c9f85ab97
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 01:45:57 +0200

    wip

--- hs-pkgs/yul-dsl-linear-smc/internal/lvm/Data/LinearContext.hs
+++ hs-pkgs/yul-dsl-linear-smc/internal/lvm/Data/LinearContext.hs
@@ -13,33 +13,5 @@ Type classes required by the linear context that works with the 'Control.Linearl
 
 -}
 module Data.LinearContext
-  ( ContextualConsumable (contextualConsume)
+  (
   ) where
--- linear-base
-import Prelude.Linear (lseq)
--- eth-abi
-import Data.SimpleNP  (NP (..))
-import Data.TupleN
-
-
---------------------------------------------------------------------------------
--- ContextualConsumable
---------------------------------------------------------------------------------
-
--- | Providing a linear context @ctx@ for consuming @a@.
-class ContextualConsumable ctx a where
-  -- | Consume @a@ linearly.
-  contextualConsume :: forall. ctx ⊸ a ⊸ ctx
-
-instance ContextualConsumable ctx () where
-  contextualConsume ctx u = lseq u ctx
-
-instance ContextualConsumable ctx (NP '[]) where
-  contextualConsume ctx Nil = ctx
-
-instance ( ContextualConsumable ctx x
-         , ContextualConsumable ctx (NP xs)
-         ) => ContextualConsumable ctx (NP (x:xs)) where
-  contextualConsume ctx (x :* xs) = let ctx' = contextualConsume ctx x
-                                        ctx'' = contextualConsume ctx' xs
-                                    in ctx''
