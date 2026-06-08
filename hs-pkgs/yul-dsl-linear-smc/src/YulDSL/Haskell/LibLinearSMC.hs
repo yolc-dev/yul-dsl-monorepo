6873f3322cdd13688f1747b4d302d92d5e3c144d
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 02:10:01 +0200

    wip

--- hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/LibLinearSMC.hs
+++ hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/LibLinearSMC.hs
@@ -14,5 +14,5 @@ import YulDSL.Haskell.Effects.LinearSMC.YulPort
 
 
 
-keccak256'l :: forall a eff r. YulO2 r a => P'x eff r a ⊸ P'x eff r B32
+keccak256'l :: forall a r. YulO2 r a => P'x PurePort r a ⊸ P'x PurePort r B32
 keccak256'l = encodeP'x (YulJmpB (MkYulBuiltIn @"__keccak_c_" @a @B32))
