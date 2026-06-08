b12c2ce3a4a942173c2ef1353e50f21d8d680982
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 16:30:33 +0200

    wip

--- hs-pkgs/yul-dsl/src/YulDSL/Core/YulBuiltIn.hs
+++ hs-pkgs/yul-dsl/src/YulDSL/Core/YulBuiltIn.hs
@@ -11,8 +11,8 @@ This module provides type class and data types for defining yul built-in functio
 -}
 {-# LANGUAGE OverloadedStrings #-}
 module YulDSL.Core.YulBuiltIn
-  ( YulBuiltInPrefix (yulB_prefix , yulB_fname, yulB_body)
-  , YulBuiltIn (MkYulBuiltIn), AnyYulBuiltIn (MkAnyYulBuiltIn)
+  ( YulBuiltInPrefix (yulB_prefix , yulB_fname)
+  , YulBuiltIn (MkYulBuiltIn)
   ) where
 -- base
 import GHC.TypeLits                (KnownSymbol (symbolSing), Symbol, fromSSymbol)
@@ -34,39 +34,8 @@ class KnownSymbol prefix => YulBuiltInPrefix (prefix :: Symbol) a b where
   -- | Yul function name for the built-in.
   yulB_fname :: forall. YulBuiltIn prefix a b -> String
   -- | Optional yul function body, which consists of in-vars, out-vars, lines of code, and built-in dependencies.
-  yulB_body :: forall. YulBuiltIn prefix a b -> ([Var], [Var], [Code], [AnyYulBuiltIn])
-  -- | Equivalent evaluation function for the built-in.
 
 -- | Reference to an instance of yul built-in by its prefix @p@, domain and codomain types @a -> b@.
 data YulBuiltIn p a b where
   MkYulBuiltIn :: forall p a b. YulBuiltInPrefix p a b => YulBuiltIn p a b
 
--- | Existential wrapper for 'YulBuiltIn'.
-data AnyYulBuiltIn where
-  MkAnyYulBuiltIn :: forall p a b. YulBuiltInPrefix p a b => YulBuiltIn p a b -> AnyYulBuiltIn
-
--- | Generate code from any yul built-in.
-yulB_code :: AnyYulBuiltIn -> (Indenter -> Code, [AnyYulBuiltIn])
-yulB_code (MkAnyYulBuiltIn p) =
-  let fname = yulB_fname p
-      (inVars, outVars, codeLines, deps) = yulB_body p
-  in (, deps) $ \ind ->
-    let ind' = indent ind
-    in ind ("function " <> T.pack fname <>
-            "(" <> spread_vars inVars <> ")" <>
-            (if null outVars then "" else " -> " <> spread_vars outVars) <>
-            " { ") <>
-       (foldr (T.append . ind') T.empty codeLines) <>
-       ind ("}")
-
-instance YulBuiltInPrefix p a b => Eq (YulBuiltIn p a b) where
-  a == b = yulB_fname a == yulB_fname b
-
-instance YulBuiltInPrefix p a b => Ord (YulBuiltIn p a b) where
-  a <= b = yulB_fname a <= yulB_fname b
-
-instance Eq AnyYulBuiltIn where
-  (MkAnyYulBuiltIn a) == (MkAnyYulBuiltIn b) = yulB_fname a == yulB_fname b
-
-instance Ord AnyYulBuiltIn where
-  (MkAnyYulBuiltIn a) <= (MkAnyYulBuiltIn b) = yulB_fname a <= yulB_fname b
--- hs-pkgs/yul-dsl/src/YulDSL/StdBuiltIns/ABICodec.hs
+++ hs-pkgs/yul-dsl/src/YulDSL/StdBuiltIns/ABICodec.hs
@@ -20,10 +20,6 @@ import YulDSL.StdBuiltIns.ValueType ()
 instance ( ABITypeable b, YulBuiltInPrefix "__cleanup_t_" U256 b
          ) => YulBuiltInPrefix "__abidec_from_calldata_t_" (U256, U256) b where
   yulB_fname b = yulB_prefix b <> abiTypeCanonName @b
-  yulB_body _ = ( MkVar <$> ["offset", "end"], [ MkVar "value" ]
-                , [ "value := " <> T.pack (yulB_fname cleanup_f) <> "(calldataload(offset))" ]
-                , [ MkAnyYulBuiltIn cleanup_f ])
-    where cleanup_f = MkYulBuiltIn @"__cleanup_t_" @U256 @b
 
 ------------------------------------------------------------------------------------------------------------------------
 
@@ -33,32 +29,13 @@ instance ( ABITypeable b, YulBuiltInPrefix "__cleanup_t_" U256 b
 
 instance ABITypeable a => YulBuiltInPrefix "__abienc_from_stack_c_" (U256, a) U256 where
   yulB_fname b = yulB_prefix b <> abiTypeCompactName @a
-  yulB_body _ =
-    let types = abiTypeInfo @a
-        aVars = gen_vars (length types)
-    in ( MkVar "headStart" : aVars, [ MkVar "tail" ]
-       , abienc_main_body abienc_from_stack_builtin_f types aVars
-       , fmap abienc_from_stack_builtin_f types)
 
 instance ( ABITypeable a, YulBuiltInPrefix "__cleanup_t_" U256 a
          ) => YulBuiltInPrefix "__abienc_from_stack_t_" (U256, a) () where
   yulB_fname b = yulB_prefix b <> abiTypeCanonName @a
-  yulB_body _ = ( MkVar <$> ["pos", "value"], []
-                , [ "mstore(pos, " <> T.pack (yulB_fname cleanup_f) <> "(value))" ]
-                , [ MkAnyYulBuiltIn cleanup_f ])
-    where cleanup_f = MkYulBuiltIn @"__cleanup_t_" @U256 @a
 
 instance ABITypeable a => YulBuiltInPrefix "__keccak_c_" a B32 where
   yulB_fname b = yulB_prefix b <> abiTypeCompactName @a
-  yulB_body _ =
-    let inVars = gen_vars (length (abiTypeInfo @a))
-        abienc_builtin = MkYulBuiltIn @"__abienc_from_stack_c_" @(U256, a) @U256
-    in ( inVars, [ MkVar "hash" ]
-       , [ " let memPos := allocate_unbounded()"
-         , " let memEnd := " <> T.pack (yulB_fname abienc_builtin) <> "(memPos, " <> spread_vars inVars <> ")"
-         , " hash := keccak256(memPos, sub(memEnd, memPos))"
-         ]
-       , [ MkAnyYulBuiltIn abienc_builtin ])
 
 ------------------------------------------------------------------------------------------------------------------------
 
@@ -68,47 +45,4 @@ instance ABITypeable a => YulBuiltInPrefix "__keccak_c_" a B32 where
 
 -- decoder
 
-abidec_from_calldata_builtin_f t = case t of
-  INTx' @s @n _ _ -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abidec_from_calldata_t_" @(U256, U256) @(INTx s n))
-  BOOL'           -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abidec_from_calldata_t_" @(U256, U256) @BOOL)
-  ADDR'           -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abidec_from_calldata_t_" @(U256, U256) @ADDR)
-  _               -> error ("abidec_from_calldata_builtin_f unsupported: " <> show t)
 
-
-abidec_main_body :: (ABICoreType -> AnyYulBuiltIn) -> [ABICoreType] -> [Var] -> [Code]
-abidec_main_body builtin_f types vars =
-  add_indent "if slt(sub(dataEnd, headStart), " <>
-  -- TODO use a better revert
-  T.pack (show dataSize) <> ") { revert(0, 0) }" :
-  map add_indent (go types 0)
-  where
-    go (n:ns) slot =
-      [ "{"
-      , add_indent $ "let offset := " <> T.pack (show (slot * 32))
-      , add_indent $ unVar (vars !! slot) <> " := "
-        <> T.pack (case builtin_f n of MkAnyYulBuiltIn b -> yulB_fname b) <> "(add(headStart, offset), dataEnd)"
-      , "}"
-      ]
-      ++ go ns (slot + 1)
-    go [] _ = []
-    dataSize = length types * 32
-
--- encoder
-
-abienc_from_stack_builtin_f t = case t of -- "__abienc_from_stack_c1_" ++ abiCoreTypeCompactName t
-  INTx' @s @n _ _ -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abienc_from_stack_t_" @(U256, INTx s n) @())
-  BOOL'           -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abienc_from_stack_t_" @(U256, BOOL) @())
-  ADDR'           -> MkAnyYulBuiltIn (MkYulBuiltIn @"__abienc_from_stack_t_" @(U256, ADDR) @())
-  _               -> error ("abienc_from_stack_builtin_f unsupported: " <> show t)
-
-abienc_main_body :: (ABICoreType -> AnyYulBuiltIn) -> [ABICoreType] -> [Var] -> [Code]
-abienc_main_body builtin_f types vars =
-  add_indent "tail := add(headStart, " <> T.pack (show dataSize) <> ")" :
-  map add_indent (go types 0)
-  where
-    go (n:ns) slot =
-      T.pack (case builtin_f n of MkAnyYulBuiltIn b -> yulB_fname b)
-      <> "(add(headStart, " <> T.pack(show (slot * 32)) <> "), " <> unVar (vars !! slot) <> ")"
-      : go ns (slot + 1)
-    go _ _         = []
-    dataSize = length types * 32
