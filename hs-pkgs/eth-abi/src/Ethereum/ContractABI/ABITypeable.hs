ab95742617c4baa8e92316fca4ba84b9a75ed177
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 21:18:55 +0200

    wip

--- hs-pkgs/eth-abi/src/Ethereum/ContractABI/ABITypeable.hs
+++ hs-pkgs/eth-abi/src/Ethereum/ContractABI/ABITypeable.hs
@@ -25,11 +25,5 @@ class ABITypeable a where
   -- | Convert @a@ to the ABI core type it derives from.
   type ABITypeDerivedOf a
 
-  -- | Returns a list of core types represented by this type.
-  --
-  -- Invariant: @abi_type_info a == abi_type_info \@(ABITypeDerivedOf a)@
   abiTypeInfo :: String
-  -- ^ The default implementation must be implemented by core types.
-
-  -- | Convert a value from the core type to the extended type.
   abiFromCoreType :: a -> a
