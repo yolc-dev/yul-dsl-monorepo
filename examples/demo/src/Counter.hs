1e9fdd7b9ef34ffd69d09e4325293c170e11ae3d
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2026-06-08 18:30:18 +0200

    combine

--- examples/demo/src/Counter.hs
+++ examples/demo/src/Counter.hs
@@ -1,8 +1,7 @@
 module Counter where
 import Prelude.Linear                           (String)
 import YulDSL.Core                              (ADDR, NP, REF, U256 )
-import  YulDSL.Core.YulCat (YulO1)
-import YulDSL.Haskell.Effects.LinearSMC.YulPort (P'P, extendType'l, lfn', keccak256'l )
+import YulDSL.Haskell.Effects.LinearSMC.YulPort (P'P, extendType'l, lfn', keccak256'l , YulO1)
 
 
 -- | Get a storage reference from the storage hash-map.
--- hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/YulPort.hs
+++ hs-pkgs/yul-dsl-linear-smc/src/YulDSL/Haskell/Effects/LinearSMC/YulPort.hs
@@ -4,6 +4,11 @@ module YulDSL.Haskell.Effects.LinearSMC.YulPort
   (P'P ,  keccak256'l
   , extendType'l
   , lfn'
+
+  , YulCat (..)
+  -- * YulCat Stringify Functions
+  , YulCatObj
+  , YulO1, YulO2, YulO3
   ) where
 import Prelude.Linear
 import Control.Category.Linear             (P, decode, encode)
@@ -17,6 +22,66 @@ import Prelude (undefined)
 import Data.Constraint              (Dict (Dict))
 -- linear-smc
 import Control.Category.Constrained (Cartesian (..), Category (..), Monoidal (..), ProdObj (..))
+
+
+import Data.Kind                    (Constraint, Type)
+
+
+
+
+-- | All objects in the yul category is simply a 'YulCatObj'.
+class (ABITypeable a, ABITypeCodec a) => YulCatObj a where
+  -- | Possible breakdown of the product object of the category.
+
+type YulO1 a = YulCatObj a
+type YulO2 a b = (YulCatObj a, YulO1 b)
+type YulO3 a b c = (YulCatObj a, YulO2 b c)
+
+--
+-- Enumerate known YulCat objects:
+--
+
+-- NP
+instance YulCatObj (NP '[])
+instance (YulCatObj x, YulCatObj (NP xs)) => YulCatObj (NP (x:xs))
+
+-- TupleN (3..15)
+instance YulCatObj ()
+instance (YulCatObj a1, YulCatObj a2) => YulCatObj (a1, a2)
+
+-- Value Types
+instance YulCatObj BOOL
+instance ValidINTx s n => YulCatObj (INTx s n)
+instance YulCatObj ADDR
+instance ValidINTn n => YulCatObj (BYTESn n)
+
+-- REF
+instance YulCatObj a => YulCatObj (REF a)
+
+
+------------------------------------------------------------------------------------------------------------------------
+-- The Cat
+------------------------------------------------------------------------------------------------------------------------
+
+-- | Use kind signature for the 'YulCat' to introduce the terminology in a lexical-orderly way.
+type YulCat ::  Type -> Type -> Type
+
+data YulCat a b where
+  YulExtendType :: forall a b. (YulO2 a b, a ~ ABITypeDerivedOf b) => YulCat a b
+  YulComp :: forall a b c.  YulCat c b %1-> YulCat a c %1-> YulCat a b
+  YulJmpB :: forall a b. ( YulO2 a b) =>  YulCat a b
+
+
+
+yulCatCompactShow :: YulCat a b -> String
+yulCatCompactShow = go
+  where
+    go :: YulCat a' b' -> String
+    go (YulExtendType  @a @b)    = "Te" <> abiTypeCompactName @b
+    go (YulComp cb ac)             = "(" <> go ac <> ");(" <> go cb <> ")"
+    go (YulJmpB  @a @b )        = "Jb "
+
+
 --
 instance ProdObj YulCatObj where
   prodobj = undefined
